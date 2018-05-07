cdc23481981594cdfd3233ac9ee590cff66674c2
An Nguyen Quynh Anh <anhanqq@gmail.com>
2018-05-07 16:50:42 +0700

    fix client message parser

--- project/lib/libnetwork/parser/parser.c
+++ project/lib/libnetwork/parser/parser.c
@@ -1,15 +1,16 @@
 #include "parser.h"
 
 MessageClient clientJsonToStruct(char * json) {
-    MessageClient msg = newMessageClient();
-    //msg.message = malloc(100 * sizeof(char));
-    //msg.user = malloc(10 * sizeof(char));
-    //msg.password = malloc(10 * sizeof(char));
+    //MessageClient msg = newMessageClient();
+    MessageClient msg;
+    msg.message = (char *) malloc(100 * sizeof(char));
+    msg.user = (char *) malloc(10 * sizeof(char));
+    msg.password = (char *) malloc(10 * sizeof(char));
 
-    json_scanf(json, strlen(json), "{method: %B}", &(msg.method));
-    json_scanf(json, strlen(json), "{sender: %B}", &(msg.sender));
-    json_scanf(json, strlen(json), "{group: %B}", &(msg.group));
-    json_scanf(json, strlen(json), "{receiver: %B}", &(msg.receiver));
+    json_scanf(json, strlen(json), "{method: %d}", &(msg.method));
+    json_scanf(json, strlen(json), "{sender: %d}", &(msg.sender));
+    json_scanf(json, strlen(json), "{group: %d}", &(msg.group));
+    json_scanf(json, strlen(json), "{receiver: %d}", &(msg.receiver));
 
     json_scanf(json, strlen(json), "{message: %Q}", &(msg.message));
     json_scanf(json, strlen(json), "{user: %Q}", &(msg.user));
@@ -18,33 +19,36 @@ MessageClient clientJsonToStruct(char * json) {
     return msg;
 }
 
-MessageServer serverJsonToStruct(char * json) {
-    MessageServer msg = newMessageServer();
-
-    json_scanf(json, strlen(json), "{method: %B}", &(msg.method));
-    json_scanf(json, strlen(json), "{message: %Q}", &(msg.message));
-    json_scanf(json, strlen(json), "{sender: %B}", &(msg.sender));
-    json_scanf(json, strlen(json), "{group: %B}", &(msg.group));
-
-    return msg;
-}
-
 char * clientStructToJson(MessageClient msg) {
     char * str;
     str = (char *) malloc(200 * sizeof(char));
-    // please free the memory after using :>
 
     struct json_out json = JSON_OUT_BUF(str, 200);
-    printf("Method: %d\n", msg.method);
+    //printf("Method: %d\n", msg.method);
     json_printf(&json, "{method: %d, sender: %d, group: %d, receiver: %d, message: %Q, user: %Q, password: %Q}", msg.method, msg.sender, msg.group, msg.receiver, msg.message, msg.user, msg.password);
     //printf("%s\n", str);
     return str;
 }
+
+MessageServer serverJsonToStruct(char * json) {
+    MessageServer msg;
+    
+    msg.message = (char *) malloc(100 * sizeof(char));
+
+    json_scanf(json, strlen(json), "{method: %B}", &(msg.method));
+    json_scanf(json, strlen(json), "{sender: %B}", &(msg.sender));
+    json_scanf(json, strlen(json), "{group: %B}", &(msg.group));
+
+    json_scanf(json, strlen(json), "{message: %Q}", &(msg.message));
+
+    return msg;
+}
+
+
+
 
 char * serverStructToJson(MessageServer msg) {
     char * str;
     str = (char *) malloc(200 * sizeof(char));
     struct json_out json = JSON_OUT_BUF(str, 200);
 }
-
-json_printf(&json, "{method: %d, message: %Q, error: %M, sender: %d, group: %d}", msg.method, msg.message, msg.error, msg.sender, msg.group);
