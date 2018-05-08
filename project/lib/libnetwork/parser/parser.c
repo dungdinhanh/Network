0541aa6958ee24d6844b6c3a8ee5c956a2e2051a
An Nguyen Quynh Anh <anhanqq@gmail.com>
2018-05-08 17:00:18 +0700

    finalize serverJsonToStruct

--- project/lib/libnetwork/parser/parser.c
+++ project/lib/libnetwork/parser/parser.c
@@ -1,4 +1,11 @@
+//#include <libconfig/message.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <regex.h>
+#include "frozen.h"
 #include "parser.h"
+#define MAX_ARRAY 100
 
 MessageClient clientJsonToStruct(char * json) {
     //MessageClient msg = newMessageClient();
@@ -15,7 +22,6 @@ MessageClient clientJsonToStruct(char * json) {
     json_scanf(json, strlen(json), "{message: %Q}", &(msg.message));
     json_scanf(json, strlen(json), "{user: %Q}", &(msg.user));
     json_scanf(json, strlen(json), "{password: %Q}", &(msg.password));
-
     return msg;
 }
 
@@ -24,9 +30,7 @@ char * clientStructToJson(MessageClient msg) {
     str = (char *) malloc(200 * sizeof(char));
 
     struct json_out json = JSON_OUT_BUF(str, 200);
-    //printf("Method: %d\n", msg.method);
     json_printf(&json, "{method: %d, sender: %d, group: %d, receiver: %d, message: %Q, user: %Q, password: %Q}", msg.method, msg.sender, msg.group, msg.receiver, msg.message, msg.user, msg.password);
-    //printf("%s\n", str);
     return str;
 }
 
@@ -34,21 +38,74 @@ MessageServer serverJsonToStruct(char * json) {
     MessageServer msg;
     
     msg.message = (char *) malloc(100 * sizeof(char));
-
-    json_scanf(json, strlen(json), "{method: %B}", &(msg.method));
-    json_scanf(json, strlen(json), "{sender: %B}", &(msg.sender));
-    json_scanf(json, strlen(json), "{group: %B}", &(msg.group));
-
+    
+    json_scanf(json, strlen(json), "{method: %d}", &(msg.method));
+    json_scanf(json, strlen(json), "{sender: %d}", &(msg.sender));
+    json_scanf(json, strlen(json), "{group: %d}", &(msg.group));
     json_scanf(json, strlen(json), "{message: %Q}", &(msg.message));
 
+    // parse error array
+    char * error = (char *) malloc(100 * sizeof(char));
+    int * errorArray = (int *) malloc(100 * sizeof(int));
+    json_scanf(json, strlen(json), "{error: %Q}", &error);
+    errorArray = parseIntegerArray(error);
+
+    int i = 0;
+    while (errorArray[i]) {
+        msg.error[i] = errorArray[i];
+        i++;
+    }
+    free(error);
+    free(errorArray);
+
+    // parse object array
+    msg.object = malloc(10 * sizeof(Object));
+    struct json_token t;
+    for (i = 0; json_scanf_array_elem(json, strlen(json), ".object", i, &t) > 0; i++) {
+        msg.object[i].user = (char *) malloc(100 * sizeof(char)); 
+        json_scanf(t.ptr, t.len, "{id: %d, user: %Q}", &msg.object[i].id, &msg.object[i].user);
+        printf("Id: %d\n", msg.object[i].id);
+        printf("User: %s\n", msg.object[i].user);
+    }
     return msg;
 }
-
-
 
 
 char * serverStructToJson(MessageServer msg) {
     char * str;
     str = (char *) malloc(200 * sizeof(char));
     struct json_out json = JSON_OUT_BUF(str, 200);
+    return str;
+}
+
+
+int *parseIntegerArray(char *stringArray) {
+    int * result = (int *)malloc(sizeof(int) * MAX_ARRAY);
+    char *s = "[,\n ";
+    char *tempString = (char *)malloc(sizeof(char) * MAX_ARRAY);
+    strcpy(tempString, stringArray);
+    char * buffer = (char *)malloc(sizeof(char) * MAX_ARRAY);
+    int count = 0;
+    buffer = strtok(tempString, s);
+    while(1) {
+        int element = atoi(buffer);
+        result[count] = element;
+        count++;
+        if((buffer= strtok(NULL, s)) == NULL)
+            break;
+    }
+    result[count] = -1;
+    return result;
+}
+
+
+void printResult(int *a) {
+    if (a == NULL)
+        return;
+    int i = 0;
+    while(a[i] != -1) {
+        printf("%d  ", a[i]);
+        i++;
+    }
+    printf("\n");
 }
